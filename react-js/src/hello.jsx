56af4ce53f9eb8a87f34fd06fcbc780350d0cd9a
MahshadOliaie <mahshadoliaie@gmail.com>
2023-11-03 23:18:52 +0330

    change btn content

--- react-js/src/hello.jsx
+++ react-js/src/hello.jsx
@@ -6,12 +6,13 @@ let i = 0;
 
 function Greeting() {
     return (
-        <p className="btn" onClick={() => {changeBg(bgs[i]); i++}}>Click me!!!</p>
+        <p className="btn" onClick={() => {changeBg(bgs[i]); if(i<4){i++}}}>click me!!!</p>
     )
 }
 export default Greeting;
 
 
 function changeBg(color) {
+    event.target.textContent = color
     document.body.style.backgroundColor = color
 }
