ab7e13e8b2255190c2cb3c6182afa8849ae432a7
MahshadOliaie <mahshadoliaie@gmail.com>
2023-11-09 13:15:45 +0330

    edit header

--- react-js/src/assets/MyHeader.jsx
+++ react-js/src/assets/MyHeader.jsx
@@ -7,9 +7,10 @@ return(
             <p href="" className="header__navbar__link">دسته بندی</p>
             <p href="" className="header__navbar__link">سبد خرید</p>
             <p href="" className="header__navbar__link">ارتباط با ما</p>
+            <img src="./src/assets/images/bars.svg" className="header__navbar__bar" alt="bars" />
         </div>
         <div className="header__logo"><img src="./src/assets/images/WhiteLogo.png" alt="logo" /></div>
-        <div className="header__register">ورود</div>
+        <div className="header__register"><p>ورود</p></div>
     </header>
 )
 }
