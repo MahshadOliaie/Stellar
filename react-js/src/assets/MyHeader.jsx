a6cccb691bbb900cabcfc23288973c7c42e48690
MahshadOliaie <mahshadoliaie@gmail.com>
2023-11-09 22:18:37 +0330

    debug

--- react-js/src/assets/MyHeader.jsx
+++ react-js/src/assets/MyHeader.jsx
@@ -4,9 +4,9 @@ function MyHeader(){
 return(
     <header className="header">
         <div className="header__navbar">
-            <p href="" className="header__navbar__link">دسته بندی</p>
-            <p href="" className="header__navbar__link">سبد خرید</p>
-            <p href="" className="header__navbar__link">ارتباط با ما</p>
+            <p className="header__navbar__link">دسته بندی</p>
+            <p className="header__navbar__link">سبد خرید</p>
+            <p className="header__navbar__link">ارتباط با ما</p>
             <img src="./src/assets/images/bars.svg" className="header__navbar__bar" alt="bars" />
         </div>
         <div className="header__logo"><img src="./src/assets/images/WhiteLogo.png" alt="logo" /></div>
