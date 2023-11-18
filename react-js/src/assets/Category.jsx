f63885006b1187f7c80db445f35fe0d0d2b7aedb
MahshadOliaie <mahshadoliaie@gmail.com>
2023-11-18 21:21:17 +0330

    edit

--- react-js/src/assets/Category.jsx
+++ react-js/src/assets/Category.jsx
@@ -1,10 +1,12 @@
 
 
-function Category(Props){
-    return(
-    <div className="category" id={Props.id}>
-        <p className="category__title">{Props.title}</p>
-    </div>
+function Category(Props) {
+    return (
+
+        <div className="category" id={Props.id}>
+            <p className="category__title">{Props.title}</p>
+        </div>
+
     )
 }
 
