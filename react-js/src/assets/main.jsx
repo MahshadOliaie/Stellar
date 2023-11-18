2629823aff6969d163d37e9440a2c8bca381acc1
MahshadOliaie <mahshadoliaie@gmail.com>
2023-11-18 21:38:21 +0330

    dynamic header style

--- react-js/src/assets/main.jsx
+++ react-js/src/assets/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import MyHeader from './MyHeader'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
+
+function stickyHeader(){
+  if(window.scrollY>100){
+    document.querySelector(".header").classList.add("scroll")
+    document.querySelector(".header__logo img").setAttribute("src" , "./src/assets/images/WhiteLogo.png")
+  }else{
+    document.querySelector(".header").classList.remove("scroll")
+    document.querySelector(".header__logo img").setAttribute("src" , "./src/assets/images/StellarLogo.png")
+  }
+}
+
+window.addEventListener("scroll", stickyHeader);
