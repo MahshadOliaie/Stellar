ba03d240d65382aa8003d39a04a68ae7faa0b7ee
MahshadOliaie <mahshadoliaie@gmail.com>
2023-12-03 17:17:11 +0330

    bag page done

--- react-js/src/assets/BagProduct.jsx
+++ react-js/src/assets/BagProduct.jsx
@@ -11,7 +11,14 @@ function BagProduct() {
                 <div className="bag__product__about__color"></div>
                 <p className="bag__product__about__size">Size: <span>XS</span></p>
                 <p className="bag__product__about__price"><span>780,000</span> T</p>
+                
+                <div className="controlQty">
                 <input className="options__qty__input bagQty" id="qty" name="qty" value={1} />
+               <div className="controlQty__operation">
+               <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
+               </div>
+                </div>
             </div>
         </div>
     )
