a74fc25b660e63518964573e603bc690aba50642
MahshadOliaie <mahshadoliaie@gmail.com>
2024-01-25 21:07:30 +0330

    add form tag in signup page

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -6,11 +6,11 @@ function SignupPage() {
     const Navigate = useNavigate()
     const [error, setError] = useState(false)
     const [inputValue, setInputValue] = useState("")
-    const [username , setUsername] = useState("")
+    const [username, setUsername] = useState("")
 
     // useEffect(()=>{
     //     if(context.user){
-       
+
     //         Navigate('/')
     //     }
 
@@ -30,7 +30,7 @@ function SignupPage() {
 
     }
 
-    function handleUsername(){
+    function handleUsername() {
         let value = event.target.value
         setUsername(value)
     }
@@ -40,10 +40,12 @@ function SignupPage() {
             <p className="backBtn" onClick={() => Navigate('/')}>back to home</p>
             <div className="form">
                 <h1 className="form__title">sign up</h1>
-                <input type="text" className="form__username" value={username} placeholder="username" onChange={handleUsername}/>
-                <input className={error && "notValid"} onChange={numberValidation} value={inputValue} type="text" placeholder="phone number" maxLength={11}/>
-                {(error) && <p className="wrong">wrong number!</p>}
-                <button type="submit" className={((error) || (inputValue.length == 0)) ? "form__submit form__submitSignup disabled" : "form__submit form__submitSignup"} onClick={() => Navigate('/codePage')}>send code</button>
+                <form action="">
+                    <input type="text" className="form__username" value={username} placeholder="username" onChange={handleUsername} />
+                    <input className={error && "notValid"} onChange={numberValidation} value={inputValue} type="text" placeholder="phone number" maxLength={11} />
+                    {(error) && <p className="wrong">wrong number!</p>}
+                    <button type="submit" className={((error) || (inputValue.length == 0)) ? "form__submit form__submitSignup disabled" : "form__submit form__submitSignup"} onClick={() => Navigate('/codePage')}>send code</button>
+                </form>
                 <p className="form__change" onClick={() => Navigate('/login')}>already have an account?</p>
             </div>
         </div>
