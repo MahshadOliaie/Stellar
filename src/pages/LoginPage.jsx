013d2fcf24d28a4f31f0396fe3d96fb23f7679fa
MahshadOliaie <mahshadoliaie@gmail.com>
2024-01-25 20:46:57 +0330

    edit login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState,useRef, useEffect } from "react";
+import { useContext, useState, useRef, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
@@ -7,23 +7,42 @@ import AuthContext from "../context/AuthContext";
 function LoginPage() {
     event.preventDefault()
     const Navigate = useNavigate()
-    const context=useContext(AuthContext)
-    const numberEl=useRef()
-    const [loginErr,setLoginErr]=useState({err:false})
-    const [qParams,setQParams]=useSearchParams()
-    useEffect(()=>{
-        if(context.user){
-      
+    const context = useContext(AuthContext)
+    const numberEl = useRef()
+    const [qParams, setQParams] = useSearchParams()
+    useEffect(() => {
+        if (context.user) {
+
             Navigate('/')
         }
-        if(qParams.has('errmessage')){
+        if (qParams.has('errmessage')) {
 
-            setLoginErr({err:true,message:qParams.get('errmessage')})
-            setQParams((prevParam)=>prevParam.delete('errmessage'))
+            setLoginErr({ err: true, message: qParams.get('errmessage') })
+            setQParams((prevParam) => prevParam.delete('errmessage'))
         }
-        
 
-    },[])
+
+    }, [])
+
+    const [error, setError] = useState(false)
+    const [inputValue, setInputValue] = useState("")
+
+
+    function checkNumber() {
+        let value = event.target.value;
+        setInputValue(value)
+
+        if ((value.length > 0 && value.length < 11) || !(value.match(/^\d{1,}$/)))
+            setError(true)
+        else
+            setError(false)
+
+        if (value.length == 0)
+            setError(false)
+
+
+
+    }
 
 
     return (
@@ -31,11 +50,15 @@ function LoginPage() {
             <p className="backBtn" onClick={() => Navigate('/')}>back to home</p>
             <div className="form">
                 <h1 className="form__title">login</h1>
-                <input ref={numberEl} onKeyUp={checkNumber} className="form__number" id="number" type="number" placeholder="phone number"/>
-                <p className="wrong dnone">wrong number!</p>
-                <p className="wrong">{loginErr.message}</p>
+                <form action="">
+                    <input className={error && "notValid"} ref={numberEl} value={inputValue} onChange={checkNumber} id="number" type="text" placeholder="phone number" maxLength={11} />
+                    {(error) && <p className="wrong">wrong number!</p>}
 
-                <button type="submit" className="form__submit disabled" onClick={() => context.sendPhoneNumber(numberEl.current.value,setLoginErr)}>send code</button>
+                    {/* <p className="wrong">{loginErr.message}</p> */}
+
+                    <button type="submit" className={((error) || (inputValue.length == 0)) ? "form__submit disabled" : "form__submit"} onClick={() => context.sendPhoneNumber(numberEl.current.value, setLoginErr)}>send code</button>
+
+                </form>
                 <p className="form__change" onClick={() => Navigate('/signUp')}>create an account</p>
             </div>
         </div>
@@ -45,30 +68,3 @@ function LoginPage() {
 export default LoginPage;
 
 
-
-export function checkNumber() {
-    let value = event.target.value;
-
-    if (value.length > 0 && value.length < 11) {
-        event.target.style.boxShadow = "inset 0px 2px 4px rgba(255, 0, 0, 0.5)";
-        document.querySelector(".wrong").classList.add("dnone")
-        document.querySelector(".form__submit").classList.add("disabled")
-
-    } else {
-        event.target.style.boxShadow = "inset 0px 2px 4px rgba(0, 0, 0, 0.25)";
-        document.querySelector(".wrong").classList.add("dnone")
-    }
-
-    if (value.length > 11) {
-        event.target.style.boxShadow = "inset 0px 2px 4px rgba(255, 0, 0, 0.5)";
-        document.querySelector(".wrong").classList.remove("dnone")
-        document.querySelector(".form__submit").classList.add("disabled")
-    }
-    if(value.length == 11){
-        document.querySelector(".form__submit").classList.remove("disabled")
-        phoneNumber = value;
-    }
-
-   
-
-}
